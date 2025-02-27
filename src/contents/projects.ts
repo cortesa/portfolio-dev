32fbc0565553c5d03174ef8cafcd07a712798ca6
Andrés Cortés <cortesa@gmail.com>
2025-02-27 14:31:35 +0100

    new project in the lab

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -17,4 +17,12 @@ export const PROJECTS: Project[] = [
       'Kanban is an online tool for managing projects and tasks using the Kanban method (not production ready). Developed by cortesa.net, it offers an intuitive interface and advanced features to improve productivity and organization.',
     techs: ['REACT', 'DNDKIT', 'TAILWIND'],
   },
+  {
+    name: 'Weather',
+    image: '/images/labWeatherPreview.webp',
+    url: 'https://weather.lab.cortesa.net',
+    description:
+      'Weather is a web application that fetches real-time weather data using the wttr.in API. Users can search for a city to view current conditions and upcoming forecasts in a clean and intuitive interface.',
+    techs: ['REACT', 'TAILWIND'],
+  },
 ]
